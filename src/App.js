2edcd5fd77c9daa9eb5e2f6532266958a011ed57
pubby <pubby8@gmail.com>
2019-11-05 17:58:45 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+class Data {
+  constructor() {
+    this.fields = {};
+    this.errors = {};
+  }
+
+  validate() {
+
+  }
+
+
+}
+
 const steps = [
   { 
     header: 'What was the crime?',
@@ -42,7 +55,7 @@ function App() {
         <div className='steps'>
           { steps.map((data, index) => {
             return (
-                <Step 
+                <Ste 
                   key={ index.toString() } 
                   stepNumber={ index+1 } 
                   { ...data } 
@@ -77,34 +90,124 @@ function stepIDName(stepNumber) { return `step-${stepNumber}`; }
 function expandedClass(expanded) { return expanded ? 'expanded' : 'collapsed'; }
 function evenOddClass(i) { return i % 2 ? 'odd' : 'even'; }
 
+function Validity({validity}) {
+  let text = '';
+  let style = {};
+
+  switch(validity) {
+  case 'valid':    text = '✓'; style = { color: 'green' }; break;
+  case 'notvalid': text = '✗'; style = { color: 'red' };   break;
+  case 'thinking': text = '…'; style = { color: 'black' }; break;
+  }
+
+  return (<span className='step-valid-status' style={style}>{text}</span>);
+}
+
 class Step extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       expanded: false,
+      validity: 'empty', // One of: { 'empty', 'notvalid', 'valid', 'thinking' }
     };
   };
   
   render() {
     return (
       <div className='step' id={stepIDName(this.props.stepNumber)}>
-        <h3 className='step-header'>{ this.props.header }</h3> 
+        <h3 className='step-header'>
+            { this.props.header }
+            <Validity validity={this.state.validity} />
+        </h3> 
+
         <p className='step-text-simple'>{ this.props.simpleText }</p> 
 
-        <p>Input fields and stuff would go here.</p>
-        <input type="text" /><br />
-        <input type="text" /><br />
-        <input type="text" /><br />
-        <p>More input stuff, etc.</p>
+        { this.renderFields() }
 
         <button 
           className={`hover step-expand-button ${ this.state.expanded ? 'minus' : 'plus' }`}
           onClick={() => { this.setState({ expanded: !this.state.expanded }); }}
-          title={`Click to see ${ this.state.expanded ? 'less' : 'more' } information about "${ this.props.navName }".`}>
+          titleNameInput={`Click to see ${ this.state.expanded ? 'less' : 'more' } information about "${ this.props.navName }".`}>
             {`${ this.state.expanded ? 'Less' : 'More' } Information`}
         </button>
         <p className={`step-text-complex ${ expandedClass(this.state.expanded) }`}>{ this.props.complexText }</p> 
       </div>
+    );
+  }
+}
+
+class InputField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  modified() {
+    this.
+  }
+
+  handleChange() {
+      const name = event.target.name;
+      const value = event.target.value;
+
+      const updatedControls = {
+        ...this.state.formControls
+      };
+      const updatedFormElement = {
+        ...updatedControls[name]
+      };
+      updatedFormElement.value = value;
+      updatedFormElement.touched = true;
+      updatedFormElement.valid = validate(value, updatedFormElement.validationRules);
+
+      updatedControls[name] = updatedFormElement;
+
+      let formIsValid = true;
+      for (let inputIdentifier in updatedControls) {
+        formIsValid = updatedControls[inputIdentifier].valid && formIsValid;
+      }
+
+      this.setState({
+        formControls: updatedControls,
+        formIsValid: formIsValid
+      });
+  }
+
+  return (
+    <input type="text" onChange={this.handleChange} />
+  );
+}
+
+class PersonalInfo extends React.Component
+{
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+    };
+  }
+
+  modified() {
+    if(this.modifiedTimer)
+      clearTimeout(this.modificationTimer);
+    this.modificationTimer = setTimeout(() => { }, 750);
+  }
+
+  update() {
+    this.props.
+
+    this.props.changedValidity();
+  }
+
+  firstNameChange(event) { this.setState({firstName: event.target.value}, modified.bind(this)); }
+  lastNameChange(event)  { this.setState({lastName: event.target.value}, modified.bind(this)); }
+
+  render() {
+    return (
+      <input type="text" value={this.state.firstName} onChange={this.firstNameChange} />
+      <input type="text" value={this.state.lastName} onChange={this.lastName} />
     );
   }
 }
