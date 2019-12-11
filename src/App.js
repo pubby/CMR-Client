d7c50d60ee01e9b35af614f29664ea9f02055254
unknown <mcarthura@live.com>
2019-12-10 18:47:14 -0600

    Added Hidden sidebar functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Step from './components/displays/step';
 import NavBar from './components/displays/sideNav';
+import Hidden from '@material-ui/core/Hidden';
 
 const Steps = require("./data/config");
 
@@ -25,20 +26,21 @@ export default function App() {
           }) }
         </div>
       </div>
-
-      <div className='navbar'>
-        <ol className='navbar-sticky'>
-          { Steps.steps.map((data, index) => {
-            return (
-              <NavBar 
-                key={ index.toString() } 
-                stepNumber={ index+1 } 
-                { ...data } 
-              />
-            );
-          }) }
-        </ol>
-      </div>
+      <Hidden smDown>
+        <div className='navbar'>
+          <ol className='navbar-sticky'>
+            { Steps.steps.map((data, index) => {
+              return (
+                <NavBar 
+                  key={ index.toString() } 
+                  stepNumber={ index+1 } 
+                  { ...data } 
+                />
+              );
+            }) }
+          </ol>
+        </div>
+      </Hidden>
 
       <div className='footer'>
           <div className='footer-content'>footer placeholder</div>
