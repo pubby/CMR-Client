c85d84bb7275c38b5e877913f5fb044eaa842326
pubby <pubby8@gmail.com>
2019-12-14 14:42:04 -0600

    First implementation of scroll tracking

--- src/components/displays/sideNav.jsx
+++ src/components/displays/sideNav.jsx
@@ -1,20 +1,49 @@
 import React from 'react';
-import { evenOddClass, stepIDName } from '../../css.js'
+import { stepIDName, inView } from '../../css.js'
 
-export default function NavBar(props) {
-  function doScroll() {
-    document.getElementById(stepIDName(props.stepNumber)).scrollIntoView({
-        behavior: 'smooth',
-        block: 'center'
-    });
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { sectionInView: true };
   }
 
-  return (
-    <li 
-      className="hover"
-      onClick={doScroll}
-    >
-      {props.navName}
-    </li>
-  );
+  render() {
+    const doScroll = () => {
+      document.getElementById(stepIDName(this.props.stepNumber)).scrollIntoView({
+          behavior: 'smooth',
+          block: 'center'
+      });
+    }
+
+    return (
+      <li 
+        className={`hover ${this.state.sectionInView ? 'navbar-in-view' : 'navbar-not-in-view'}`}
+        onClick={doScroll}
+      >
+        {this.props.navName}
+      </li>
+    );
+  }
+    
+  componentDidMount() {
+    const callback = (entries, observer) => { 
+      window.requestIdleCallback(() => {
+        this.setState({ sectionInView: entries[entries.length - 1].isIntersecting });
+      });
+    };
+
+    const options = {
+      threshold: 0.2,
+    };
+
+    this.observer = new IntersectionObserver(callback, options);
+    this.observer.observe(document.getElementById(stepIDName(this.props.stepNumber)));
+  }
+
+  componentWillUnmount() {
+    this.observer.disconnect();
+    this.observer = null;
+  }
 }
+
+
