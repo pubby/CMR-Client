d148df45b65469d07525aae6aaed76b74f118248
pubby <pubby8@gmail.com>
2019-12-14 13:52:17 -0600

    Updates

---
+++ precache-manifest.60cc39040c08a503a1861e4abf1a6ab3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e1c6b0be4eb53b39b785e6f7638f2e",
+    "url": "/CMR-Client/index.html"
+  },
+  {
+    "revision": "5715087ae5de0dac397b",
+    "url": "/CMR-Client/static/css/main.af00bd43.chunk.css"
+  },
+  {
+    "revision": "1f9049c6783f95f90f2c",
+    "url": "/CMR-Client/static/js/2.b98f4124.chunk.js"
+  },
+  {
+    "revision": "5715087ae5de0dac397b",
+    "url": "/CMR-Client/static/js/main.7435fa2a.chunk.js"
+  },
+  {
+    "revision": "f76f7bcbebee9ee78adc",
+    "url": "/CMR-Client/static/js/runtime-main.6530780d.js"
+  }
+]);
